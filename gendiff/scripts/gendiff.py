95e9ef3cdc77143a8b67e06e4e9a5fe18ef3a55b
Nikita <Nikita.Clark@yandex.ru>
2020-09-25 15:26:55 +0300

    checking .gitingore

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,22 @@
 import argparse
 import json
 from gendiff.comparisons import generate_diff
+import os
 
 
 def main():
+    print('Hello')
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     arg1 = args.first_file
+    if arg1[0] != '/':
+        arg1 = os.getcwd() + '/' + arg1
     arg2 = args.second_file
+    if arg2[0] != '/':
+        arg2 = os.getcwd() + '/' + arg2
     diff = generate_diff(arg1, arg2)
     print(diff)
     
