c017b1d657301426696feb058fe616db5807869e
Nikita <Nikita.Clark@yandex.ru>
2020-10-13 19:42:37 +0300

    code review 1

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,23 +2,23 @@ from gendiff.build_diff import generate_diff
 import sys
 import pytest
 
-way = sys.path[0]
-file_path1_json = way + '/fixtures/file1.json'
-file_path2_json = way + '/fixtures/file2.json'
-file_path1_yaml = way + '/fixtures/file1.yaml'
-file_path2_yaml = way + '/fixtures/file2.yaml'
+way = sys.path[0] + '/fixtures/'
+file_path1_json = way + 'file1.json'
+file_path2_json = way + 'file2.json'
+file_path1_yaml = way + 'file1.yaml'
+file_path2_yaml = way + 'file2.yaml'
 
-new_file_path1_json = way + '/fixtures/new_file1.json'
-new_file_path2_json = way + '/fixtures/new_file2.json'
+new_file_path1_json = way + 'new_file1.json'
+new_file_path2_json = way + 'new_file2.json'
 
 
-with open(way + '/fixtures/answer1.txt', 'r') as file:
+with open(way + 'answer1.txt', 'r') as file:
     answer1 = file.read()
 
-with open(way + '/fixtures/answer2.txt', 'r') as file:
+with open(way + 'answer2.txt', 'r') as file:
     answer2 = file.read()
 
-with open(way + '/fixtures/answer3.txt', 'r') as file:
+with open(way + 'answer3.txt', 'r') as file:
     answer3 = file.read()
 
 
