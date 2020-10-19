24c2d0229e1ca57338ec6341e803fa8362cca547
Nikita <Nikita.Clark@yandex.ru>
2020-10-19 17:35:47 +0300

    test file

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,19 +4,18 @@ import pytest
 
 way = sys.path[0] + '/fixtures/'
 
-with open(way + 'answer1.txt', 'r') as file:
-    answer1 = file.read()
 
-with open(way + 'answer2.txt', 'r') as file:
-    answer2 = file.read()
-
-with open(way + 'answer3.txt', 'r') as file:
-    answer3 = file.read()
+def open_file(name):
+    with open(way + name, 'r') as file:
+        return file.read()
 
 
-data = [(way + 'file1.json', way + 'file2.json', 'string', answer1),
-        (way + 'file1.yaml', way + 'file2.yaml', 'string', answer1),
-        (way + 'new_file1.json', way + 'new_file2.json', 'string', answer2)]
+data = [(way + 'file1.json', way + 'file2.json', 'string',
+         open_file('answer1.txt')),
+        (way + 'file1.yaml', way + 'file2.yaml', 'string',
+         open_file('answer1.txt')),
+        (way + 'new_file1.json', way + 'new_file2.json', 'string',
+         open_file('answer2.txt'))]
 
 
 @pytest.mark.parametrize('file_path1, file_path2, f, expected', data)
@@ -26,7 +25,8 @@ def test_string_format(file_path1, file_path2, f, expected):
 
 def test_plain_format():
     assert generate_diff(way + 'new_file1.json',
-                         way + 'new_file2.json', 'plain') == answer3
+                         way + 'new_file2.json',
+                         'plain') == open_file('answer3.txt')
 
 
 def test_json_format():
