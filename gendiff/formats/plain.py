cabff95cf0c7c490bbd889efed4f1b23e9943dbb
Nikita <Nikita.Clark@yandex.ru>
2020-10-26 10:23:23 +0300

    code refactoring

--- gendiff/formats/plain.py
+++ gendiff/formats/plain.py
@@ -18,18 +18,16 @@ def make_format(data, way):
         if state == 'nested':
             result += make_format(value, way+str(key))
         if state == 'deleted':
-            result += make_property(state, key, way) + '\n'
+            result += ("Property '" + way + str(key) + "' " + states[state]
+                       + '\n')
         if state == 'added':
-            result += make_property(state, key, way) + to_str(value) + '\n'
+            result += ("Property '" + way + str(key) + "' " + states[state]
+                       + to_str(value) + '\n')
         if state == 'changed':
             value, new_value = value
-            result += (make_property(state, key, way) + to_str(value) + ' to '
-                       + to_str(new_value) + '\n')
+            result += ("Property '" + way + str(key) + "' " + states[state]
+                       + to_str(value) + ' to ' + to_str(new_value) + '\n')
     return result
-
-
-def make_property(state, key, way):
-    return "Property '" + way + str(key) + "' " + states[state]
 
 
 def to_str(value):
