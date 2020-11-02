13cd095cf7c687ad25dc3406af51a2cab0ca3c53
Nikita <Nikita.Clark@yandex.ru>
2020-11-02 18:04:28 +0300

    code coverage

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,9 @@
 from gendiff.build_diff import generate_diff
-from gendiff.cli import parser
+from gendiff.cli import get_parser
 
 
 def main():
-    parser = parser()
+    parser = get_parser()
     args = parser.parse_args()
     diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
